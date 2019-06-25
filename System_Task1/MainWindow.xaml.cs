313facab6c6e40b145a1aa654c53c2407811d358
nurlan3295 <nurlan3295@gmail.com>
2019-06-25 21:48:56 +0400

    25.06.2019

--- System_Task1/MainWindow.xaml.cs
+++ System_Task1/MainWindow.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Path = System.IO.Path;
 
 namespace System_Task1
 {
@@ -21,12 +22,61 @@ namespace System_Task1
     /// </summary>
     public partial class MainWindow : Window
     {
-            string[] droppedFiles = null;
+
+        static void DirSearch(string sDir,List<string> list)
+        {
+            string data = String.Empty;
+            try
+            {
+                foreach (string d in Directory.GetDirectories(sDir))
+                {
+                    foreach (string f in Directory.GetFiles(d))
+                    {
+                        // Console.WriteLine(f);
+                      //  MessageBox.Show(f);
+                        //Console.WriteLine("__________________________________________");
+                        if (f.Contains(".txt"))
+                        {
+                            using (StreamReader sr = new StreamReader(f))
+                            {
+
+                                data = sr.ReadToEnd();
+                            }
+                            foreach (var item in list)
+                            {
+                            var hasWord = data.Contains(item);
+                            if (hasWord)
+                            {
+
+                                  MessageBox.Show("yes");
+                                  string  fileName = Path.GetFileName(f);
+                                    string[] pathArr = fileName.Split('.');
+                                    File.Copy(f, "\\\\STHQ01DC01\\dfr$\\Must_xm12\\Desktop\\"+$"{pathArr[0]}"+ DateTime.Now.ToString("ff")+"."+pathArr[1]);
+                                    string path= ("\\\\STHQ01DC01\\dfr$\\Must_xm12\\Desktop\\" + $"{pathArr[0]}" + DateTime.Now.ToString("ff") + "Copy." + pathArr[1]);
+                                    File.Copy(f, path);
+                                    string text = File.ReadAllText(path);
+                                    text = text.Replace($"{item}", $"**{item}**");
+                                    File.WriteAllText($"{path}", text);
+                                }
+                            }
+                        }
+                    }
+                    DirSearch(d,list);
+                }
+            }
+            catch (System.Exception excpt)
+            {
+                //    Console.WriteLine(excpt.Message);
+              MessageBox.Show("Excpt"+excpt.Message);
+            }
+        }
+
+        string[] droppedFiles = null;
         public MainWindow()
         {
             InitializeComponent();
         }
-        List<string> List = new List<string>();
+       List<string> List = new List<string>();
       
 
         private void Ellipse_Drop(object sender, DragEventArgs e)
@@ -42,8 +92,7 @@ namespace System_Task1
                 {
                     List.Add(Name);
                 }
-                string[] filePaths = Directory.GetFiles(@"C:\", "*.txt",
-                                         SearchOption.TopDirectoryOnly);
+                DirSearch(@"C:\Users\Must_xm12\source\repos\System_Task12",List);
             }
         }
     }
